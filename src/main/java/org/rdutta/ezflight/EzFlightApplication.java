b87d753eda17eec276322facffe02ddce35ce4ae
angKafka <angdevlab@outlook.com>
2025-07-19 22:41:17 +0530

    first commit

---
+++ src/main/java/org/rdutta/ezflight/EzFlightApplication.java
@@ -0,0 +1,13 @@
+package org.rdutta.ezflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzFlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzFlightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rdutta/ezflight/EzFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rdutta.ezflight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzFlightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
